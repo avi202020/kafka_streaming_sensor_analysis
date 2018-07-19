a201f0305d8b07a80b09b7960591bbfd97af1382
Paul Harris <paulharris@austins-mbp.lan>
2018-07-19 13:59:07 -0400

    Changed schema format for all sensors. Each reading is sent to the topic as with its sensorID as its key. Each sensor is now written to its own partition.

--- src/main/java/com/pattersonconsultingtn/kafka/examples/PollutionDataTracker/PollutionSensorProducer.java
+++ src/main/java/com/pattersonconsultingtn/kafka/examples/PollutionDataTracker/PollutionSensorProducer.java
@@ -10,6 +10,8 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import java.util.Date;
 import java.util.Properties;
 import java.util.Random;
+import java.util.Map;
+import java.util.HashMap;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
@@ -21,41 +23,41 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 /**
- 
+
  Quick Start
- 
+
  # Start Zookeeper. Since this is a long-running service, you should run it in its own terminal.
  $ ./bin/zookeeper-server-start ./etc/kafka/zookeeper.properties
- 
+
  # Start Kafka, also in its own terminal.
  $ ./bin/kafka-server-start ./etc/kafka/server.properties
- 
+
  # Start the Schema Registry, also in its own terminal.
  ./bin/schema-registry-start ./etc/schema-registry/schema-registry.properties
- 
- 
- 
+
+
+
  // Create topic in Kafka
- 
+
  ./bin/kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 \
- --partitions 1 --topic pollution_sensor_reading
- 
- 
+ --partitions 1 --topic air_quality_sensors_topic
+
+
  // Run the producer from maven
- 
+
  mvn exec:java -Dexec.mainClass="com.pattersonconsultingtn.kafka.examples.PollutionDataTracker.PollutionSensorProducer" \
  -Dexec.args="10 http://localhost:8081"
- 
- 
- 
+
+
+
  // check topic for entries
- ./bin/kafka-avro-console-consumer --zookeeper localhost:2181 --topic pollution_sensor_reading --from-beginning
+ ./bin/kafka-avro-console-consumer --zookeeper localhost:2181 --topic air_quality_sensors_topic --from-beginning
  */
 
 public class PollutionSensorProducer {
-    
-    private static final String CSV_FILE_PATH = "/PattersonConsulting/kafka_examples/AirQualityUCI.csv";
-    
+
+    private static final String CSV_FILE_PATH = "/Users/paulharris/code/kafka_streaming_sensor_analysis/src/main/resources/AirQualityUCI.csv";
+
     public static void main(String[] args){
         if (args.length != 2) {
             System.out.println("Please provide command line arguments: numEvents schemaRegistryUrl");
@@ -63,7 +65,7 @@ public class PollutionSensorProducer {
         }
         long events = Long.parseLong(args[0]);
         String url = args[1];
-        
+
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
         props.put("acks", "all");
@@ -71,64 +73,57 @@ public class PollutionSensorProducer {
         props.put("key.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
         props.put("value.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
         props.put("schema.registry.url", url);
-        
-        
-        String topicName = "air_quality_reading";
-        String topicKey = "sensor_0"; // sensorID
-        
+
+
+        Map<String, String> sensorIds = new HashMap<String, String>();
+        sensorIds.put("PT08S1", "PT08.S1(CO)");
+        sensorIds.put("NMHC", "NMHC(GT)");
+        sensorIds.put("PT08S2", "PT08.S2(NMHC)");
+        sensorIds.put("NOx", "NOx(GT)");
+        sensorIds.put( "PT08S3", "PT08.S3(NOx)");
+        sensorIds.put("NO2", "NO2(GT)");
+        sensorIds.put("PT08S4", "PT08.S4(NO2)");
+        sensorIds.put("PT08S5", "PT08.S5(O3)");
+
+        //Message Key
+        String topicName = "air_quality_sensors_topic";
+
         String schemaString = "{\"namespace\": \"example.avro\", " +
         "\"type\": \"record\", " +
         "\"name\": \"" + topicName + "\"," +
         "\"fields\": [" +
         "{\"name\": \"date\", \"type\": \"string\"}," +
         "{\"name\": \"time\", \"type\": \"string\"}," +
-        "{\"name\": \"PT08S1\", \"type\": \"string\"}," +
-        "{\"name\": \"NMHC\", \"type\": \"string\"}," +
-        "{\"name\": \"PT08S2\", \"type\": \"string\"}," +
-        "{\"name\": \"NOx\", \"type\": \"string\"}," +
-        "{\"name\": \"PT08S3\", \"type\": \"string\"}," +
-        "{\"name\": \"NO2\", \"type\": \"string\"}," +
-        "{\"name\": \"PT08S4\", \"type\": \"string\"}," +
-        "{\"name\": \"PT08S5\", \"type\": \"string\"}" +
+        "{\"name\": \"sensor\", \"type\": \"string\"}," +
+          "{\"name\": \"reading\", \"type\": \"string\"}" +
         "]}";
-        
+
         Producer<String, GenericRecord> producer = new KafkaProducer<String, GenericRecord>(props);
-        
+
         Schema.Parser parser = new Schema.Parser();
         Schema schema = parser.parse( schemaString );
         Reader reader = null;
         CSVParser csvParser = null;
-        
+
         try {
             reader = Files.newBufferedReader(Paths.get(CSV_FILE_PATH));
             csvParser = new CSVParser(reader, CSVFormat.DEFAULT
                                       .withFirstRecordAsHeader());
             for (CSVRecord csvRecord : csvParser) {
+              for(String key: sensorIds.keySet()){
                 String date = csvRecord.get("Date");
                 String time = csvRecord.get("Time");
-                String pts1 = csvRecord.get("PT08.S1(CO)");
-                String nmhc = csvRecord.get("NMHC(GT)");
-                String pts2 = csvRecord.get("PT08.S2(NMHC)");
-                String nogt = csvRecord.get("NOx(GT)");
-                String pts3 = csvRecord.get("PT08.S3(NOx)");
-                String no2gt = csvRecord.get("NO2(GT)");
-                String pts4 = csvRecord.get("PT08.S4(NO2)");
-                String pts5 = csvRecord.get("PT08.S5(O3)");
-                
-                
+                String reading = csvRecord.get(sensorIds.get(key));
+
                 GenericRecord reading_record = new GenericData.Record(schema);
                 reading_record.put("date", date);
                 reading_record.put("time", time);
-                reading_record.put("PT08S1", pts1);
-                reading_record.put("NMHC", nmhc);
-                reading_record.put("PT08S2", pts2);
-                reading_record.put("NOx", nogt);
-                reading_record.put("PT08S3", pts3);
-                reading_record.put("NO2", no2gt);
-                reading_record.put("PT08S4", pts4);
-                reading_record.put("PT08S5", pts5);
-                ProducerRecord<String, GenericRecord> data = new ProducerRecord<String, GenericRecord>( topicName, topicKey, reading_record );
+                reading_record.put("sensor", key);
+                reading_record.put("reading", reading);
+
+                ProducerRecord<String, GenericRecord> data = new ProducerRecord<String, GenericRecord>( topicName, key, reading_record );
                 producer.send(data);
+              }
             }
         }catch(Exception e) {
             System.out.println(e);
@@ -136,4 +131,3 @@ public class PollutionSensorProducer {
         producer.close();
     }
 }
-
